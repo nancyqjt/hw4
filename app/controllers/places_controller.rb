89b2a60acf901662645b05a1243d7220e7285c7d
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-03-03 03:26:39 +0000

    Places Controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,10 +14,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
-    redirect_to "/places"
+    if @current_user
+      @place = Place.new
+      @place["name"] = params["place"]["name"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
+      redirect_to "/places"
   end
-
 end
