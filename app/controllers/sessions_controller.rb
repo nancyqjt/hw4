fc915bacd66f470e8d9404b7625fabd854bbb335
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-03-03 00:33:02 +0000

    Authentication Done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
     if @user
         if BCrypt::Password.new(@user["password"]) == params["password"]
             session["user_id"] = @user["id"]
-            flash["notice"] = "You logged in!"
-            redirect_to "/companies"
+            flash["notice"] = "Welcome, #{@user["username"]}."
+            redirect_to "/places"
         else
             flash["notice"] = "Your email or password is incorrect."
             redirect_to "/sessions/new"
