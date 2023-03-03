a8f2f58389426ed7663d161012bae3b3b42ff24b
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-03-03 00:09:13 +0000

    controller done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,30 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+            session["user_id"] = @user["id"]
+            flash["notice"] = "You logged in!"
+            redirect_to "/companies"
+        else
+            flash["notice"] = "Your email or password is incorrect."
+            redirect_to "/sessions/new"
+        end
+    else
+        flash["notice"] = "Your email or password is incorrect."
+        redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye. You are logged out."
+    redirect_to "/sessions/new"
   end
+
 end
   
